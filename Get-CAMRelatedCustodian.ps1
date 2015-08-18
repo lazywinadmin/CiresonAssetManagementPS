f6248045d7d2cac5251602dffa239c2d6889ef62
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-08-18 00:26:36 -0400

    Add Get-CAMRelatedCustodian

---
+++ Get-CAMRelatedCustodian.ps1
@@ -0,0 +1,30 @@
+﻿Function Get-CAMRelatedCustodian
+{
+<#
+	.SYNOPSIS
+		Function to retrieve the Related Custodian of an object
+	
+	.DESCRIPTION
+		Function to retrieve the Related Custodian of an object
+	
+	.PARAMETER WorkItemObject
+		Specifies the Object to process
+	
+	.EXAMPLE
+		PS C:\> Get-CAMRelatedCustodian -WorkItemObject $SupportContract
+	
+	.NOTES
+		Francois-Xavier Cat
+		www.lazywinadmin.com
+		@lazywinadm
+#>
+	[CmdletBinding()]
+	param (
+		[Parameter(ValueFromPipeline)]
+		$WorkItemObject
+	)
+	# Custodian RelationShip
+	$OwnedByUser_relclass_id = 'cbb45424-b0a2-72f0-d535-541941cdf8e1'
+	$OwnedByUser_relclass_obj = Get-SCSMRelationshipClass -id $OwnedByUser_relclass_id
+	Get-SCSMRelatedObject -SMObject $WorkItemObject -Relationship $OwnedByUser_relclass_obj
+}
