1a77a7d168d78314347b6ba75f3404d3bff0d7d7
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-08-18 00:32:41 -0400

    Add 2 functions
    Get-CAMSoftwareAsset
    New-CAMVendor

---
+++ Get-CAMSoftwareAsset.ps1
@@ -0,0 +1,53 @@
+﻿function Get-CAMSoftwareAsset
+{
+	<#
+	.SYNOPSIS
+		Function to retrieve Software Asset from the Cireson Asset Management
+	.DESCRIPTION
+		Function to retrieve Software Asset from the Cireson Asset Management
+	.EXAMPLE
+		Get-CamSoftwareAsset
+	.EXAMPLE
+		Get-CamSoftwareAsset -DisplayName "*Pro" 
+	.EXAMPLE
+		Get-CamSoftwareAsset -id '3cbgg558-a09c-b717-2401-05aef430b01f'
+	.NOTES
+		Francois-Xavier Cat
+		@Lazywinadm
+		www.lazywinadmin.com
+	#>
+	[CmdletBinding(DefaultParameterSetName = 'All')]
+	PARAM (
+		[Parameter(ParameterSetName = 'DisplayName')]
+		[System.String]$DisplayName,
+		
+		[Parameter(ParameterSetName = 'ID')]
+		$Id,
+		
+		$Filter
+	)
+	BEGIN
+	{
+		if (-not (Get-Module -Name SMLets)) { Import-Module -Name SMLets -ErrorAction Stop }
+		Write-Debug -Message "ParameterSetName: $($PSCmdlet.ParameterSetName)"
+		Write-Debug -Message "Parameters: $($PSBoundParameters.Keys)"
+	}
+	PROCESS
+	{
+		TRY
+		{
+			$Splatting = @{
+				Class = (get-scsmclass -name 'Cireson.AssetManagement.SoftwareAsset')
+			}
+			
+			IF ($PSBoundParameters['DisplayName']) { get-scsmobject @Splatting -Filter "DisplayName -like $DisplayName" }
+			ELSEIF ($PSBoundParameters['ID']) { get-scsmobject @Splatting -Filter "Id -eq $ID" }
+			ELSE { get-scsmobject @Splatting }
+		}
+		CATCH
+		{
+			Write-Error -Message "[PROCESS] An Error occured while querying Software Assets"
+			$Error[0].Exception.Message
+		}
+	}
+}
---
+++ New-CAMVendor.TempPoint.ps1
@@ -0,0 +1,50 @@
+﻿#get-scsmobject -class (get-scsmclass -name 'Cireson.AssetManagement.Vendor') | fl *
+#(get-scsmobject -class (get-scsmclass -name 'Cireson.AssetManagement.Vendor')).values
+
+<#
+Name                                         : VMWare
+VendorAddress1                               : Parnel House, Barrack Square
+VendorAddress2                               : Main St
+VendorCity                                   : Ballincollig
+VendorState                                  : Co, Cork
+VendorPostCode                               : 
+VendorCountry                                : Ireland
+VendorContact                                : Lauren Berquist
+VendorEmail                                  : ar@wmware.com
+VendorPhone                                  : +353-21-4660000
+VendorFax                                    : 
+WebSite                                      : www.vmware.com
+ObjectStatus                                 : Active
+AssetStatus                                  : 
+Notes                                        : 
+DisplayName                                  : VMWare
+ClassName                                    : Cireson.AssetManagement.Vendor
+LastModified                                 : 8/3/2015 2:18:21 PM
+TypeName                                     : Cireson.AssetManagement.Vendor
+Path                                         : 
+FullName                                     : Cireson.AssetManagement.Vendor:VMWare
+ManagementPackClassIds                       : {f26c94f2-1045-3d60-4c1f-59b8cbfe9931}
+LeastDerivedNonAbstractManagementPackClassId : f26c94f2-1045-3d60-4c1f-59b8cbfe9931
+TimeAdded                                    : 8/3/2015 6:18:21 PM
+LastModifiedBy                               : 7431e155-3d9e-4724-895e-c03ba951a352
+Values                                       : {VMWare, Parnel House, Barrack Square, Main St, Ballincollig...}
+IsNew                                        : False
+HasChanges                                   : False
+Id                                           : d85104a5-bc3a-f629-84ef-15f41d21a798
+ManagementGroup                              : SCSM WB Games Montreal
+ManagementGroupId                            : 0ddb97c1-4a81-3958-ad92-26cb0457c075
+GroupsAsDifferentType                        : False
+ViewName                                     : ManagedEntityGenericView
+ObjectMode                                   : All
+
+#>
+
+Function New-CAMVendor
+{
+	PARAM($Name)
+	
+	$properties = @{
+		Name = $Name
+	}
+	New-SCSMObject -Class (get-scsmclass -name 'Cireson.AssetManagement.Vendor') -PropertyHashtable $properties
+}
