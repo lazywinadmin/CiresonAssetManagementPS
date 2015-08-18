5ec58d73aad85b818d30669736c6e0edb0b2e15a
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-08-17 23:50:47 -0400

    Add Get-CAMClass

---
+++ Get-CAMClass.ps1
@@ -0,0 +1,34 @@
+﻿function Get-CAMClass
+{
+	<#
+	.SYNOPSIS
+		Function to retrieve Cireson Asset Management classes
+	.DESCRIPTION
+		Function to retrieve Cireson Asset Management classes
+	.EXAMPLE
+		Get-CAMClass
+	.EXAMPLE
+	
+	.NOTES
+		Francois-Xavier Cat
+		@Lazywinadm
+		www.lazywinadmin.com
+	#>
+	PARAM
+	(
+	<#
+		[Parameter(ValueFromPipelineByPropertyName = $true,
+				   ValueFromPipeline = $true)]
+		[Alias("Computer","__SERVER","CN")]
+		[String[]]$ComputerName,
+		
+		[Alias("RunAs")]
+		[System.Management.Automation.Credential()]
+		$Credential = [System.Management.Automation.PSCredential]::Empty,
+	
+		$Name
+	#>
+	)
+	Get-ScsmClass -name 'Cireson.AssetManagement.*'
+}
+
