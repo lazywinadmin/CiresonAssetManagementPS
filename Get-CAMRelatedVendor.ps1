757dcb4ac2b4c979a98e18f664b0e25f29253a97
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2015-08-18 00:28:37 -0400

    Add Get-CAMRelatedVendor

---
+++ Get-CAMRelatedVendor.ps1
@@ -0,0 +1,32 @@
+﻿Function Get-CAMRelatedVendor
+{
+<#
+	.SYNOPSIS
+		Function to retrieve the Vendor associate with a specific CAM class
+	
+	.DESCRIPTION
+		Function to retrieve the Vendor associate with a specific CAM class.
+		I'm using this for Support Contracts.
+	
+	.PARAMETER WorkItemObject
+		Specify the Object
+	
+	.EXAMPLE
+		PS C:\> Get-CAMRelatedVendor -WorkItemObject $SupportContract
+	
+	.NOTES
+		Francois-Xavier Cat
+		www.lazywinadmin.com
+		@lazywinadm
+#>
+	[CmdletBinding()]
+	PARAM (
+		[Parameter(ValueFromPipeline)]
+		$WorkItemObject
+	)
+	
+	# Vendor RelationShip
+	$Vendor_relclass_id = '74a61725-6305-bdd4-327a-fd49e1ccf993'
+	$Vendor_relclass_obj = Get-SCSMRelationshipClass -id $Vendor_relclass_id
+	Get-SCSMRelatedObject -SMObject $WorkItemObject -Relationship $Vendor_relclass_obj
+}
